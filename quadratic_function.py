313a47d1574389fe0e2639b3fa0c95c017091c2e
arcibyte <arcibyte@gmail.com>
2025-08-21 12:42:03 -0500

    ✨ feat: define quadratic function f(x) with parameters a, b, c

--- quadratic_function.py
+++ quadratic_function.py
@@ -1,2 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Parámetros de la función cuadrática
+a = -3   # Coeficiente cuadrático
+b = -2   # Coeficiente lineal
+c = 1    # Término independiente
+
+# Definición de la función polinomial
+def f(x):
+    return a * x**2 + b * x + c
